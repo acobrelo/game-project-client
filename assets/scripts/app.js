fea551b3b4a60709fd4f3f5fdb90a0e8d6ebba98
acobrelo <acobrelo@gmail.com>
2016-06-01 13:13:51 -0400

    Removed slash, testing
    Same issue, slash removed. This is a test?

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  host: 'http://tic-tac-toe.wdibos.com/',
+  host: 'http://tic-tac-toe.wdibos.com',
 };
 
 const app = {
-  host: 'http://tic-tac-toe.wdibos.com/',
+  host: 'http://tic-tac-toe.wdibos.com',
 };
 
 module.exports = app;
